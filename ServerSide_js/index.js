30abf0c0882b451847c4243f56805e5052594bd4
Zahra Khan <zahrakhan@Zahras-Air.fios-router.home>
2022-10-15 19:25:56 -0400

    added more html files

--- ServerSide_js/index.js
+++ ServerSide_js/index.js
@@ -17,11 +17,45 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public',  'index.html'))
   })
 
+  app.get('/Zahra', (req, res) => {
+    res.sendFile(path.join(__dirname, './public',  'Zahra.html'))
+})
 
-  app.listen(port, () => {
+app.get('/Iverson', (req, res) => {
+    res.sendFile(path.join(__dirname, './public',  'Iverson.html'))
+})
+
+app.get('/Benjamin', (req, res) => {
+    res.sendFile(path.join(__dirname, './public',  'Benjamin.html'))
+})
+
+app.get('/Joe', (req, res) => {
+    res.sendFile(path.join(__dirname, './public',  'Joe.html'))
+})
+
+app.get('/Fajer', (req, res) => {
+    res.sendFile(path.join(__dirname, './public',  'Fajer.html'))
+})
+
+
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
 
-  app.post('/', (req, res) => {
-    res.send('Recieved a post')
+app.post('/', (req, res) => {
+    res.send('Recieved a post request')
   })
+
+app.post('/comment', (req, res) => {
+    res.send('Got the data')
+})
+
+
+//   app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+//   })
+
+//   app.post('/', (req, res) => {
+//     res.send('Recieved a post')
+//   })
